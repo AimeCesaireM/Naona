936be4709a87deafae9bf4c81883ad8d54c80a89
Aime Cesaire <mu.aimecesaire@gmail.com>
2024-02-19 16:26:58 +0100

    Added functionality for stop video and mute buttons

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+const { text } = require("express");
+
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myVideo = document.createElement('video');
@@ -13,7 +15,7 @@ let myVideoStream
 navigator.mediaDevices.getUserMedia({
     //get an AV stream
     video: true,
-    audio: false
+    audio: true
 
     }).then(stream => {
         console.log ("test 1")
@@ -32,6 +34,19 @@ navigator.mediaDevices.getUserMedia({
         socket.on('user-connected', (userId) => {
             connectToNewUser(userId, stream)
         })
+    let msg = $('input')
+
+    $('html').keydown( (e) => {
+        if (e.which == 13 && text.val().length !== 0){
+            socket.emit('message', text.val())
+            text.val('')
+        }
+    })
+
+    socket.on('createMessage', message => {
+        $('.messages').append(`<li class="message"><b>user</b><br/>${message})</li>`)
+        scrollToBottom()
+    })
 
 })
 
@@ -58,4 +73,67 @@ const addVideoSttream = (videoElement, stream) => {
         videoElement.play();
     })
    videoGrid.append(videoElement)
+}
+
+const scrollToBottom = () => {
+    var d = $('.main__chat_window')
+    d.scrollTop(d.prop("scrollHeight")) 
+}
+
+//mute onclick function
+const muteUnmute = () =>{
+    console.log(clicked)
+    const enabled = myVideoStream.getAudioTracks()[0].enabled
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false
+        setUnmuteButton()
+
+    } else {
+        setMuteButton()
+        myVideoStream.getAudioTracks()[0].enabled = true
+    }
+}
+
+const setUnmuteButton = () => {
+    const html = `
+        <i class="mute fa-solid fa-microphone-slash"></i>
+        <span>Unmute</span>
+    `
+    document.getElementsByClassName("main__mute_button").innerHTML = html
+}
+
+const setMuteButton = () => {
+    const html = `
+        <i class="fa-solid fa-microphone"></i>
+        <span>Mute</span>
+    `
+    document.getElementsByClassName("main__mute_button").innerHTML = html
+}
+
+const playStop = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled
+    if (enabled){
+        myVideoStream.getVideoTracks()[0].enabled = false
+        setPlayVideo()
+    }
+    else {
+        setStopVideo()
+        myVideoStream.getVideoTracks()[0].enabled = true
+    }
+}
+
+const setStopVideo = () =>{
+    const html = `
+        <i class="fa-solid fa-video"></i>
+        <span>Stop Video</span>
+        `
+    document.querySelector(".main__video_button").innerHTML = html
+}
+
+const setPlayVideo = () =>{
+    const html = `
+        <i class="stop fa-solid fa-video-slash"></i>
+        <span>Play Video</span>
+        `
+    document.querySelector(".main__video_button").innerHTML = html
 }
--- server.js
+++ server.js
@@ -23,11 +23,13 @@ app.get('/:room', (req, res) => {
     res.render('room', {roomId: req.params.room})
 })
 
-// handler for (implied) join-room event 
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId)
         socket.broadcast.to(roomId).emit('user-connected', userId)
+        socket.on('message', message => {
+            io.to(roomId).emit('createMessage', message)
+        })
     })
 })
 
